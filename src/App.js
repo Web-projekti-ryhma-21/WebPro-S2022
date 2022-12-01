c28beb276049e35676c56f684fc85f678098b1d8
henjus <80738089+henjus@users.noreply.github.com>
2022-12-01 22:40:32 +0200

    graph including v3 and v4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Visualization01 from './Visual01';
+import Visualization3n4 from './Visual3n4';
 
 function App() {
   return (
     <div className="App">
-      <Visualization01/>
+      <Visualization01 />
+      <Visualization3n4 />
     </div>
   );
 }
--- src/server/index.js
+++ src/server/index.js
@@ -83,4 +83,64 @@ app.get("/southernhemispheremonthly",async function (req,res){
     }
 
 })
+app.get("/v3annnual",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('SELECT * FROM v3annual')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+
+})
+app.get("/v3monthly",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('SELECT * FROM v3monthly')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+
+})
+app.get("/v4ica1",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('SELECT * FROM v4ica1')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+
+})
+app.get("/v4ica2",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('SELECT * FROM v4ica2')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+
+})
+app.get("/v4ica3",async function (req,res){
+    try{
+        const connection = await mysql.createConnection(config.db)
+        const [result] = await connection.execute('SELECT * FROM v4ica3')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err){
+        res.status(500).json({error:err.message})
+    }
+
+})
 app.listen(port)
