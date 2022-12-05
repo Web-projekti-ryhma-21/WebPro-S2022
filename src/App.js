3399eb009481e253d7833737aba33f7657b388a4
henjus <80738089+henjus@users.noreply.github.com>
2022-12-05 22:04:12 +0200

    v5 chart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Visualization01 from './Visual01';
 import Visualization3n4 from './Visual3n4';
+import Visualization05 from './Visual05';
 
 function App() {
   return (
     <div className="App">
       <Visualization01 />
       <Visualization3n4 />
+      <Visualization05 />
     </div>
   );
 }
