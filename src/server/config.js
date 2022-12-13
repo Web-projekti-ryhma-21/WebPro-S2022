f44810ded81dd85d48868f6c7f165b17f96467cf
henjus <80738089+henjus@users.noreply.github.com>
2022-12-13 03:06:08 +0200

    cloud deployment work

--- src/server/config.js
+++ src/server/config.js
@@ -1,10 +1,10 @@
 const config = {
     client:"mysql",
     db : {
-        user: 'testuser',
-        password: 'D14B45189EB91817A0B167A0AD401BD539B58C994C2C42D88C80E7BD39F37840',
-        database: process.env.DEV_DB_NAME || 'webpro-s2022-db',
-        socketPath: '/cloudsql/webpro-s2022:europe-west1:webpro-s2022-db'
+        user: "testuser",
+        password: "cdJ5xFjBUaug.=-B",
+        database: "webpro-s2022-db",
+        socketPath: "/cloudsql/webpro-s2022:europe-west1:webpro-s2022-db",
     }
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -16,8 +16,8 @@ const app = express()
 
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
 app.use(cors())
+app.use(bodyParser.json());
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.urlencoded({extended:false}))
