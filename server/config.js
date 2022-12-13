eed7c813f9c18fe6856072d717470a9a5b3f1064
henjus <80738089+henjus@users.noreply.github.com>
2022-12-13 18:41:52 +0200

    rearranged folder structure, bug fixes to server login disabled on server side so it'll run on GAE

---
+++ server/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db : {
+        user: "testuser",
+        password: "cdJ5xFjBUaug.=-B",
+        database: "webpro-s2022-db",
+        socketPath: "/cloudsql/webpro-s2022:europe-west1:webpro-s2022-db",
+    },
+};
+
+module.exports = config;
